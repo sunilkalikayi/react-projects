0b3e64c713c0e6747bffbd14a02abbd409e742c2
sunilkalikayi2580@gmail.com <sunilkalikayi2580@gmail.com>
2024-01-31 10:09:41 +0530

    setup: inintial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+import "./App.css";
+
+
+function App() {
+    return <div>MY </div>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
+import { store } from "./redux/store.ts";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/rock-paper-scissor",
+        element: <div>Hey game</div>,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+    </React.StrictMode>
+);
