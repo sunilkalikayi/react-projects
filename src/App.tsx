37f94220929235aa002ce296b297929238a902da
sunilkalikayi2580@gmail.com <sunilkalikayi2580@gmail.com>
2024-02-05 20:33:10 +0530

    feat: style the app

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,25 @@ import "./App.css";
 function App() {
     const navigate = useNavigate();
     return (
-        <section>
-            <h1>Have fun</h1>
-            <section>
+        <section className="m-4">
+            <p className="text-[32px] font-semibold my-4">kalikayi Projects</p>
+            <section className="flex gap-4">
                 <button
                     onClick={() => {
                         navigate("/rock-paper-scissor");
                     }}
+                    
+                    className="w-[220px] h-[100px] flex items-center justify-center bg-[#E5E5E5] rounded-[15px] "
                 >
                     Rock-Paper-Scissor
+                </button>
+                <button
+                    onClick={() => {
+                        navigate("/dail-pad");
+                    }}
+                    className="w-[220px] h-[100px] flex items-center justify-center bg-[#E5E5E5] rounded-[15px] "
+                >
+                    Number Dail Pad 
                 </button>
             </section>
         </section>
